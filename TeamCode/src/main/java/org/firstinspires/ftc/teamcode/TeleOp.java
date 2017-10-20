3f582c49a955d3c7930e8659b188e848e743714a
FTC406 <32903808+FTC406@users.noreply.github.com>
2017-10-20 08:49:53 -0500

    Commented code so that we can teach it to Caroline and Josiah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class TeleOp extends LinearOpMode
 {
 
+    //Create variables and hardware
     DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
     Float left, right;
 
@@ -18,16 +19,21 @@ public class TeleOp extends LinearOpMode
     public void runOpMode() throws InterruptedException
     {
 
+        //Initilization Procedures
+        //Configuration in the phone, this allows the motors to control physical objects that the phone is connected to
         leftDriveF = hardwareMap.dcMotor.get("leftDriveF");
         leftDriveB = hardwareMap.dcMotor.get("leftDriveB");
         rightDriveF = hardwareMap.dcMotor.get("rightDriveF");
         rightDriveB = hardwareMap.dcMotor.get("rightDriveB");
 
-        waitForStart();
 
+        waitForStart(); //Program is setup by everything above this, wait until play is pressed on the phone
+
+        //Assign values to the doubles using the gamepad values
         left = gamepad1.left_stick_y;
         right = gamepad1.right_stick_y;
 
+        //Assign power values to the motors, using the values we just made with the gamepad lines
         leftDriveF.setPower(left);
         leftDriveB.setPower(left);
         rightDriveF.setPower(right);
