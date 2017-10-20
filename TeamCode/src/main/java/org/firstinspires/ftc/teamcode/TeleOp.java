c7138ad23626fcbb07648364ce089de02de0c2cb
FTC406 <32903808+FTC406@users.noreply.github.com>
2017-10-20 07:48:25 -0500

    Created a four motor drive train teleop. Should enable functionality of pre-mecanum chassis

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Austin on 10/19/2017.
+ */
+
+public class TeleOp extends LinearOpMode
+{
+
+    DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
+    Float left, right;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        leftDriveF = hardwareMap.dcMotor.get("leftDriveF");
+        leftDriveB = hardwareMap.dcMotor.get("leftDriveB");
+        rightDriveF = hardwareMap.dcMotor.get("rightDriveF");
+        rightDriveB = hardwareMap.dcMotor.get("rightDriveB");
+
+        waitForStart();
+
+        left = gamepad1.left_stick_y;
+        right = gamepad1.right_stick_y;
+
+        leftDriveF.setPower(left);
+        leftDriveB.setPower(left);
+        rightDriveF.setPower(right);
+        rightDriveB.setPower(right);
+    }
+}
