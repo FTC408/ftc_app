274b926d103b6097b394f05f6af1d7c4a3adc953
FTC406 <32903808+FTC406@users.noreply.github.com>
2017-10-20 08:31:48 -0500

    Testing login

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -12,7 +12,7 @@ public class TeleOp extends LinearOpMode
 
     DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
     Float left, right;
-    double x;
+    
 
     @Override
     public void runOpMode() throws InterruptedException
