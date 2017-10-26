122315f7ddaed2f191912859664b6fa7586c8873
psouthrobotics1182 <zoghbya8828@parkwayschools.net>
2017-10-26 16:35:10 -0500

    Created a test mecanum class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechnumTest.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Robotics on 10/26/2017.
+ */
+@TeleOp(name="mechnum", group="Linear Opmode")
+public class mechnumTest extends LinearOpMode {
+
+    //Create variables and hardware
+    DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
+    Float left, right;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        //Initilization Procedures
+        //Configuration in the phone, this allows the motors to control physical objects that the phone is connected to
+        leftDriveF = hardwareMap.dcMotor.get("leftDriveF");
+        leftDriveB = hardwareMap.dcMotor.get("leftDriveB");
+
+        leftDriveB.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDriveF.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rightDriveF = hardwareMap.dcMotor.get("rightDriveF");
+        rightDriveB = hardwareMap.dcMotor.get("rightDriveB");
+
+
+        waitForStart(); //Program is setup by everything above this, wait until play is pressed on the phone
+
+        while (opModeIsActive())
+        {
+            //Assign values to the doubles using the gamepad values
+            left = -gamepad1.left_stick_y;
+            right = gamepad1.right_stick_y;
+
+            //Assign power values to the motors, using the values we just made with the gamepad lines
+            leftDriveF.setPower(left);
+            leftDriveB.setPower(left);
+            rightDriveF.setPower(right);
+            rightDriveB.setPower(right);
+            double magnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y,2)+Math.pow(gamepad1.left_stick_x,2));
+
+            if (Math.PI/4 < Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) && Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) < (3*Math.PI)/4)
+                leftDriveF.setPower(magnitude);
+                leftDriveB.setPower(magnitude);
+
+
+            if ( (3*Math.PI/4) < Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) && Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) < (5*Math.PI)/4)
+            {
+                leftDriveF.setPower(magnitude);
+                leftDriveB.setPower(magnitude);
+            }
+
+            if ((5*Math.PI/4) < Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) && Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) < (7*Math.PI)/4)
+            {
+                leftDriveF.setPower(magnitude);
+                leftDriveB.setPower(magnitude);
+            }
+
+            if ((7*Math.PI/4) < Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) && Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) < (Math.PI)/4)            {
+                leftDriveF.setPower(magnitude);
+                leftDriveB.setPower(magnitude);
+            }
+
+
+        }
+    }
+}
+
