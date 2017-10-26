ba669b87f2c2b110dd16144082f7ef4c27de32a0
FTC406 <32903808+FTC406@users.noreply.github.com>
2017-10-26 15:49:46 -0500

    Created autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Austin on 10/25/2017.
+ */
+@Autonomous(name= "Auto", group = "Auto")
+public class auto extends LinearOpMode
+{
+
+    //Create variables and hardware
+    DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
+    Float left, right;
+
+    public void runOpMode() throws InterruptedException
+    {
+        //Initilization Procedures
+        //Configuration in the phone, this allows the motors to control physical objects that the phone is connected to
+        leftDriveF = hardwareMap.dcMotor.get("leftDriveF");
+        leftDriveB = hardwareMap.dcMotor.get("leftDriveB");
+
+        rightDriveB.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDriveF.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rightDriveF = hardwareMap.dcMotor.get("rightDriveF");
+        rightDriveB = hardwareMap.dcMotor.get("rightDriveB");
+
+        leftDriveF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDriveB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDriveF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDriveB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        forward(1.0);
+        sleep(2000);
+        forward(0);
+
+        leftPower(1.0);
+        rightPower(-1.0);
+        sleep (1000);
+        forward(0);
+
+
+
+    }
+
+    public void leftPower(double power)
+    {
+        leftDriveB.setPower(power);
+        leftDriveF.setPower(power);
+    }
+
+    public void rightPower(double power)
+    {
+        rightDriveF.setPower(power);
+        rightDriveB.setPower(power);
+    }
+
+    public void forward(double power)
+    {
+        leftPower(power);
+        rightPower(power);
+    }
+
+    public int tickstomm(int ticks)
+    {
+        return 0;
+    }
+}
