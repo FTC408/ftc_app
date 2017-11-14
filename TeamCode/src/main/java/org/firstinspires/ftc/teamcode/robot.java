3e85d64d58d60915b474d2279f0943ffc4a6e993
FTC406 <32903808+FTC406@users.noreply.github.com>
2017-11-14 15:54:14 -0600

    Created robot class to condense code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,177 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Austin on 11/14/2017.
+ */
+
+public class robot extends LinearOpMode
+{
+    DcMotor leftDriveF, leftDriveB, rightDriveF, rightDriveB;
+    ColorSensor color;
+    CRServo intakeRight, intakeLeft;
+    Float left, right;
+    double ticksPerRev = 288;
+    double gearRatio = 1.33;
+    double diameter = 101.6;
+
+    public void init(int zed){
+    //Initilization Procedures
+    //Configuration in the phone, this allows the motors to control physical objects that the phone is connected to
+
+    leftDriveF = hardwareMap.dcMotor.get("leftDriveF");
+    leftDriveB = hardwareMap.dcMotor.get("leftDriveB");
+
+    rightDriveF = hardwareMap.dcMotor.get("rightDriveF");
+    rightDriveB = hardwareMap.dcMotor.get("rightDriveB");
+
+    intakeRight = hardwareMap.crservo.get("intakeRight");
+    intakeLeft = hardwareMap.crservo.get("intakeLeft");
+
+    //color = hardwareMap.colorSensor.get("color");
+
+    //color.enableLed(false);
+
+        rightDriveB.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDriveF.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftDriveF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDriveB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDriveF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDriveB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftDriveF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDriveF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+    public void leftPower(double power)
+    {
+        leftDriveB.setPower(power);
+        leftDriveF.setPower(power);
+    }
+
+    public void rightPower(double power)
+    {
+        rightDriveF.setPower(power);
+        rightDriveB.setPower(power);
+    }
+
+    public void forward(double power)
+    {
+        leftPower(power);
+        rightPower(power);
+    }
+
+    public void forward(double power, int mm)
+    {
+        int pos = leftDriveF.getCurrentPosition();
+        //Forward 1 meter to test encoders once we get the chance to use them
+
+        if (power >= 0) {
+            while (leftDriveF.getCurrentPosition() < (pos + mmtoticks(Math.abs(mm)))) {
+                forward(power);
+            }
+        }
+        else
+        {
+            while (leftDriveF.getCurrentPosition() > (pos - mmtoticks(Math.abs(mm)))) {
+                forward(power);
+            }
+        }
+    }
+
+    //Positive Degrees to the right, negative to the left
+    public void turn(double power, int degrees)
+    {
+        double diameter = 245 * 2; //245 mm radius
+        int pos = leftDriveF.getCurrentPosition();
+        double Circumfrence = Math.PI * diameter;
+        double distance = (degrees * Circumfrence) /180;
+
+        if (degrees >= 0) {
+            while (leftDriveF.getCurrentPosition() < (pos + mmtoticks(distance))) {
+                right(Math.abs(power));
+            }
+        }
+        else
+        {
+            while (leftDriveF.getCurrentPosition() > (pos - mmtoticks(distance))) {
+                left(Math.abs(power));
+            }
+        }
+    }
+
+    //Positive power is to the right, negative to the left
+    public void strafe(double power, int mm)
+    {
+        int pos = leftDriveF.getCurrentPosition();
+        //Forward 1 meter to test encoders once we get the chance to use them
+
+        //Strafe Right
+        if (power >= 0) {
+            while (leftDriveF.getCurrentPosition() < (pos + mmtoticks(Math.abs(mm)))) {
+                strafeRight(Math.abs(power));
+            }
+        }
+        //Strafe left
+        else {
+            while (leftDriveF.getCurrentPosition() > (pos - mmtoticks(Math.abs(mm)))) {
+                strafeLeft(Math.abs(power));
+            }
+        }
+    }
+
+    public void strafeRight(double power)
+    {
+        leftDriveF.setPower(power);
+        leftDriveB.setPower(-power);
+        rightDriveF.setPower(-power);
+        rightDriveB.setPower(power);
+    }
+
+    public void strafeLeft(double power)
+    {
+        leftDriveF.setPower(-power);
+        leftDriveB.setPower(power);
+        rightDriveF.setPower(power);
+        rightDriveB.setPower(-power);
+    }
+
+    public void right(double power)
+    {
+        leftPower(power);
+        rightPower(-power);
+    }
+    public void left(double power)
+    {
+        leftPower(-power);
+        rightPower(power);
+    }
+
+    public double tickstomm(int ticks){
+
+        double mm = (ticks * 2.955);
+        return mm;
+    }
+
+    //Go forward for such and such distance
+    public double mmtoticks(double mm)
+    {
+        double ticks = (mm / 2.955);
+        return ticks;
+    }
+
+
+
+
+
+    public void runOpMode(){}
+}
